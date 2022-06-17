3d0c33d050d41a759ddbcf8911ef9eda9212c080
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2022-06-17 16:31:44 +0100

    adding a  search box

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-
 const root = ReactDOM.createRoot( document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    
-    <Hello greeting={'Hello'+' react ninjaye'} />
-
+ <App/>
   </React.StrictMode>
 );
 
