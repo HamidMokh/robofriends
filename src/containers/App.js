1db0a372f282f637560fae2d2842988dd1a8ad45
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2022-06-18 01:03:31 +0100

    build

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from './SearchBox'
-import Scroll from './Scroll'
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
 import './App.css'
 
 
@@ -24,13 +24,14 @@ class App extends Component{
         this.setState({searchField: event.target.value});
     }
     render(){
-        const filteredRobots = this.state.robots.filter(robots=>{
-            return robots.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
+        const {robots, searchField}=this.state;
+        const filteredRobots = robots.filter(robot=>{
+            return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
         })
-        if(this.state.robots.length===0){
-            return <h1>Loading...</h1>
-        }else
-        {return(
+
+        return !robots.length?
+            <h1>Loading...</h1>:
+        (
             
             <div className="tc">
             <h1 className="f2">RoboFriends</h1>
@@ -42,6 +43,6 @@ class App extends Component{
             }
        
     }
-}
+
 
 export default App
